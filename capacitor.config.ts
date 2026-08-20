4b34723d2514ec35cb2fe1bbbb9a33b16fe01dc3
umarzaman7777777-cloud <umarzaman7777777@gmail.com>
2026-08-20 19:55:21 +0500

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.amirtraders.app',
-  appName: 'Amir Traders',
-  webDir: 'www'
-};
-
-export default config;
